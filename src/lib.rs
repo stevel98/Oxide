b16c72eae192c61e86a8214faa83648fc35a7a13
lizhixun <lizhixun@berkeley.edu>
2017-12-29 23:53:31 -0800

    prelim block skeleton

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,60 @@
+extern crate time;
 
-struct Blockchain {
+pub struct Blockchain {
     tail: Block,
 }
 
-struct Block {
+pub struct Block {
+    index: u32,
+    hash: str,
     parent_hash: str,
+    timestamp: str,
+    meta: Metadata
+}
+
+impl Block {
+    fn new(parent: Block) -> Block {
+        let new_block = Block {
+            parent.index() + 1,
+            hash: "",
+            parent_hash: parent.hash(),
+            timestamp: "", // get current time
+            meta: Meta::new() // new metadata
+        }
+        // set hash?
+    }
+
+    fn index(&self) -> u32 { //no lifetime annotation b/c u32 implements Move trait?
+        self.index
+    }
+
+    fn hash(&self) -> &str {
+        self.hash
+    }
+
+    fn parent_hash(&self) {
+        self.parent_hash
+    }
+
+    fn timestamp(&self) {
+        self.timestamp
+    }
+}
+
+impl Block {
+
+    /** Serialize myself. */
+    fn serialize(&self) {
+    }
+}
+
+struct Metadata {
+}
+
+impl Metadata {
+    fn new() -> Metadata {
+        Metadata{}
+    }
 }
 
 struct Transaction {
@@ -17,3 +67,4 @@ mod tests {
         assert_eq!(2 + 2, 4);
     }
 }
+
